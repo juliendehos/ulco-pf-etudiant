46e36a5d4403dcb09b1a441f24f57d30428cd580
Julien Dehos <dehos@univ-littoral.fr>
2020-12-07 13:59:44 +0100

    projet todoweb

---
+++ projets/todoweb/app/Main.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- import Control.Monad (when)
+import Control.Monad.IO.Class (liftIO)
+import Data.IORef
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Web.Scotty
+
+type Task =  String
+
+type Model = [Task]
+
+initialModel :: Model
+initialModel = 
+    [ "aller chercher le pain"
+    , "arrêter le changement climatique"
+    ]
+
+main :: IO ()
+main = do
+
+    modelRef <- newIORef initialModel
+
+    scotty 3000 $ do
+
+        middleware logStdoutDev
+
+        get "/" $ file "public/index.html"
+
+        get "/tasks" $ do
+            tasks <- liftIO $ readIORef modelRef
+            json tasks
+
+        post "/add" $ do
+            label <- param "label"
+            liftIO $ putStrLn ("TODO add -> " ++ label)
+            redirect "/"
+
