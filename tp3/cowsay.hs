2b67549b5e3ca4a083167b23b8c35610b517ab59
Julien Dehos <dehos@univ-littoral.fr>
2021-12-14 11:04:14 +0100

    tp3 cowsay

---
+++ tp3/cowsay.hs
@@ -0,0 +1,14 @@
+
+cow :: String
+cow = 
+    "    \\   ^__^ \n\
+    \     \\  (oo)\\_______ \n\
+    \        (__)\\       )\\/\\ \n\
+    \            ||----w | \n\
+    \            ||     || "
+
+main :: IO ()
+main = do
+    putStrLn cow
+
+
