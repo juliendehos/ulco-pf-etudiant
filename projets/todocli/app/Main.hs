bef67ea1e9c0d1687285cabcba47cfa93537a112
Julien Dehos <dehos@univ-littoral.fr>
2022-01-19 07:53:01 +0100

    update todocli

--- projets/todocli/app/Main.hs
+++ projets/todocli/app/Main.hs
@@ -1,14 +1,18 @@
-import Text.Read
-import System.Environment
-import System.IO
+-- import Text.Read
+-- import System.Environment
+-- import System.IO
 
-type Task = (Int, Bool, String)
-type Todo = (Int, [Task])
+type Task = (Int, Bool, String)     -- id, done?, label
+type Model = (Int, [Task])          -- next id, tasks
+
+test :: Model -> Model
+test (nextI, tasks) = (nextI, reverse tasks)
 
 main :: IO ()
 main = do
     contents <- readFile "tasks.txt"
-    let todo1 = read $! contents :: Todo
-    print todo1
-    mapM_ print (snd todo1)
+    let model1 = read contents
+        model2 = test $! model1
+    print model2
+    mapM_ print (snd model2)
 
